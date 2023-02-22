feab1658b034a8fba8d9823abe111dea3667270f
visakh <visakhpayyannur1997@gmail.com>
2023-02-22 19:02:38 +0530

    second commit

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -6,42 +6,33 @@ import { Injectable } from '@angular/core';
 })
 export class ApiServiceService {
 
-  constructor( private http: HttpClient) { }
-apiArray :any=[];
-getArray:any=[];
- min:number=0;
- count=0;
-  getAll(min:number,max:number,url:string){
-   this.getArray=[];
-    this.http.get(url).subscribe((res)=>{
-    this.apiArray=res;
-   
-    console.log(min," after  / ",max);
-    console.log('  getArray 1',this.getArray)
-    
-    if(min < this.apiArray.length){
-      this.getArray.splice(0,this.getArray.length)
-      if(max > this.apiArray.length){
-      
-         max=this.apiArray.length;
-         this.count=this.apiArray.length-min;
-         min= this.apiArray.length- this.count;
-        console.log(min," after  / ",max);
+  constructor(private http: HttpClient) { }
+  apiArray: any = [];
+  getArray: any = [];
+  min: number = 0;
+  count = 0;
+  getAll(min: number, max: number, url: string) {
+    this.getArray = [];
+    this.http.get(url).subscribe((res) => {
+      this.apiArray = res;
+
+      if (min < this.apiArray.length) {
+        this.getArray.splice(0, this.getArray.length)
+        if (max > this.apiArray.length) {
+          max = this.apiArray.length;
+          this.count = this.apiArray.length - min;
+          min = this.apiArray.length - this.count;
+        }
+
+        for (let index = min; index < max; index++) {
+          this.getArray.push(this.apiArray[index]);
+
+        }
+
       }
-      console.log('  getArray 2 ',this.getArray)
-      
-    for (let index = min; index < max; index++) {
-     this.getArray.push(this.apiArray[index]);
-      
-    }
-   
-   
-    }
-   
     })
-    console.log('  getArray 3',this.getArray)
     return this.getArray;
-  
+
   }
 
 }
